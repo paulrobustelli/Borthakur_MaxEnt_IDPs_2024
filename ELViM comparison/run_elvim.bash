490b12d1371e30c71e9d2dfa5e0e26d5bc658c8e
tommysisk <85724985+tommysisk@users.noreply.github.com>
2024-09-30 13:54:36 -0700

    Update run_elvim.bash

--- ELViM comparison/run_elvim.bash
+++ ELViM comparison/run_elvim.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 pdb="/dartfs-hpc/rc/lab/R/RobustelliP/Kaushik/Paper_Data/PaaA2/PaaA2.pdb"
-python3 elvim.py -f /dartfs-hpc/rc/lab/R/RobustelliP/Kaushik/Paper_Data/PaaA2/a99SBdisp/PaaA2-a99SBdisp-Traj.dcd -t $pdb -o /dartfs-hpc/rc/lab/R/RobustelliP/Tommy/kaush/elvim/elvim.paa2.a99
-python3 elvim.py -f /dartfs-hpc/rc/lab/R/RobustelliP/Kaushik/Paper_Data/PaaA2/Charmm36m/PaaA2-Charmm36m-Traj.dcd -t $pdb -o /dartfs-hpc/rc/lab/R/RobustelliP/Tommy/kaush/elvim/elvim.paa2.c36
+dcd="/dartfs-hpc/rc/lab/R/RobustelliP/Kaushik/Paper_Data/PaaA2/a99SBdisp/PaaA2-a99SBdisp-Traj.dcd"
+output_file = "/dartfs-hpc/rc/lab/R/RobustelliP/Tommy/kaush/elvim/elvim.paa2.a99"
+python3 elvim.py -f "$dcd" -t "$pdb" -o "$output_file"
