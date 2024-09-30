0d5b83a41d42a1f6134fd559c53ff7df314ca810
tommysisk <85724985+tommysisk@users.noreply.github.com>
2024-09-30 14:33:31 -0700

    Update run_elvim.bash

--- ELViM comparison/run_elvim.bash
+++ ELViM comparison/run_elvim.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
-pdb="/dartfs-hpc/rc/lab/R/RobustelliP/Kaushik/Paper_Data/PaaA2/PaaA2.pdb"
-dcd="/dartfs-hpc/rc/lab/R/RobustelliP/Kaushik/Paper_Data/PaaA2/a99SBdisp/PaaA2-a99SBdisp-Traj.dcd"
-output_file = "/dartfs-hpc/rc/lab/R/RobustelliP/Tommy/kaush/elvim/elvim.paa2.a99"
-python3 elvim.py -f "$dcd" -t "$pdb" -o "$output_file"
+
+# this is an example script -- first 4 variables should be replaced with your own files
+
+# first we need to concatenate the trajecory files we wish to analyze together with ELViM
+dcds="path/to/numpy/array/containing/paths_to_protein_simulation_files.npy"
+pdb="path/to/protein.pdb"
+dscr="string_with_protein_name" # the output concatenated simulation file will be named "{dscr}_cat.dcd"
+stride="int" # stride for simulation data
+
+./cat_trajs.py "$dcds" "$pdb" "$dscr" "$stride"
+
+# now we compute ELViM
+concatenated_traj = ""$dscr"_cat.dcd" # this will be generated from cat_trajs.npy and does not need to be modified
+
+elvim_output_file = ""$dscr"_elvim_cat.txt"
+
+python3 elvim.py -f "$dcd" -t "$pdb" -o "$elvim_output_file"
